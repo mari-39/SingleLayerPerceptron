1407440875a5a1c877a5a441e912d21bb29f020e
mari-39 <150397932+mari-39@users.noreply.github.com>
2024-09-12 18:28:27 +0200

    Deleted old src directory

--- src/Perceptron.java
+++
@@ -1,9 +0,0 @@
-public class Perceptron {
-    double[][] inputs;
-    double[] weights;
-
-    public Perceptron(double[][] inputs, double[] weights) {
-        this.inputs = inputs;
-        this.weights = weights;
-    }
-}
